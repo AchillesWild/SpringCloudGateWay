4858fbbc72d907fc80b701e97a04ca876eaf3cc1
Achilles <532398999@qq.com>
2023-10-28 22:49:10 +0800

    new

--- src/main/java/com/achilles/MyTestApplication.java
+++ src/main/java/com/achilles/MyTestApplication.java
@@ -1,14 +1,8 @@
 package com.achilles;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
-@EnableFeignClients
-@EnableAsync
 public class MyTestApplication {
 
 	public static void main(String[] args) {
