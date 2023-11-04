6401f2b34bf2cbfd152fb5a85f52e8c2cd1b448d
Achilles <532398999@qq.com>
2023-11-05 00:28:37 +0800

    gateway

---
+++ src/main/java/com/achilles/GateApplication.java
@@ -0,0 +1,17 @@
+package com.achilles;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GateApplication.class, args);
+
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~GATE SUCCESS ! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+	}
+
+}
