7c40561f18e4a7aaf9440536c9740d457f44abdc
Achilles <532398999@qq.com>
2024-01-01 22:20:17 +0800

    DELETE USELESS

--- src/main/java/com/achilles/config/WebSessionConfig.java
+++ src/main/java/com/achilles/config/WebSessionConfig.java
@@ -1,28 +1,28 @@
-package com.achilles.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseCookie;
-import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
-import org.springframework.web.server.session.CookieWebSessionIdResolver;
-import org.springframework.web.server.session.WebSessionIdResolver;
-
-import java.util.function.Consumer;
-
-@Configuration
-@EnableRedisWebSession(maxInactiveIntervalInSeconds = 10*60*60, redisNamespace = "my:spring:session")
-public class WebSessionConfig {
-
-    @Bean
-    public WebSessionIdResolver webSessionIdResolver() {
-        CookieWebSessionIdResolver resolver = new MyCookieWebSessionIdResolver();
-        resolver.setCookieName("SESSIONID");
-
-        Consumer<ResponseCookie.ResponseCookieBuilder> consumer = responseCookieBuilder -> {
-            responseCookieBuilder.path("/");
-        };
-        resolver.addCookieInitializer(consumer);
-        return resolver;
-    }
-
-}
+//package com.achilles.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.ResponseCookie;
+//import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
+//import org.springframework.web.server.session.CookieWebSessionIdResolver;
+//import org.springframework.web.server.session.WebSessionIdResolver;
+//
+//import java.util.function.Consumer;
+//
+//@Configuration
+//@EnableRedisWebSession(maxInactiveIntervalInSeconds = 10*60*60, redisNamespace = "my:spring:session")
+//public class WebSessionConfig {
+//
+//    @Bean
+//    public WebSessionIdResolver webSessionIdResolver() {
+//        CookieWebSessionIdResolver resolver = new MyCookieWebSessionIdResolver();
+//        resolver.setCookieName("SESSIONID");
+//
+//        Consumer<ResponseCookie.ResponseCookieBuilder> consumer = responseCookieBuilder -> {
+//            responseCookieBuilder.path("/");
+//        };
+//        resolver.addCookieInitializer(consumer);
+//        return resolver;
+//    }
+//
+//}
--- src/main/java/com/achilles/controller/DefaultHystrixController.java
+++ src/main/java/com/achilles/controller/DefaultHystrixController.java
@@ -1,18 +1,15 @@
-package com.achilles.controller;
-
-import com.achilles.model.response.DataResult;
-import com.achilles.model.response.code.BaseResultCode;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@Slf4j
-public class DefaultHystrixController {
-    @RequestMapping("/fallback")
-    public DataResult<Object> fallback(){
-
-        //log.error("触发熔断......");
-        return DataResult.baseFail(BaseResultCode.FAIL.code,"GateWay fallback ***********");
-    }
-}
+//package com.achilles.controller;
+//
+//import com.achilles.model.response.DataResult;
+//import com.achilles.model.response.code.BaseResultCode;
+//
+//@RestController
+//@Slf4j
+//public class DefaultHystrixController {
+//    @RequestMapping("/fallback")
+//    public DataResult<Object> fallback(){
+//
+//        //log.error("触发熔断......");
+//        return DataResult.baseFail(BaseResultCode.FAIL.code,"GateWay fallback ***********");
+//    }
+//}
