eb299ad06cf5239cfb069d0d75a382bfcaa08fb6
Achilles <532398999@qq.com>
2024-01-07 16:18:47 +0800

    log filter

--- src/main/java/com/achilles/common/GateWayFilter.java
+++ src/main/java/com/achilles/common/GateWayFilter.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -20,7 +21,16 @@ public class GateWayFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
-        log.info("server.port : " + serverPort );
-        return chain.filter(exchange);
+        long startTime = System.currentTimeMillis();
+
+        ServerHttpRequest request =exchange.getRequest();
+        String path = request.getPath().toString();
+
+        Mono<Void> mono = chain.filter(exchange);
+
+        long duration = System.currentTimeMillis() - startTime;
+        log.info("gateway.port : " + serverPort + ", " + path + " --> " + duration+"ms");
+
+        return mono;
     }
 }
