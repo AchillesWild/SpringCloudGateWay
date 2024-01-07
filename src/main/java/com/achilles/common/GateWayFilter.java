09a96ab07088f509da9e6e2aaba71d4ec062d85d
Achilles <532398999@qq.com>
2024-01-07 18:20:04 +0800

    log filter3

--- src/main/java/com/achilles/common/GateWayFilter.java
+++ src/main/java/com/achilles/common/GateWayFilter.java
@@ -5,7 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -13,7 +17,7 @@ import reactor.core.publisher.Mono;
 import java.net.InetSocketAddress;
 
 @Component
-public class GateWayFilter implements GlobalFilter {
+public class GateWayFilter implements GlobalFilter, Ordered {
 
     private final static Logger log = LoggerFactory.getLogger(GateWayFilter.class);
 
@@ -27,13 +31,22 @@ public class GateWayFilter implements GlobalFilter {
 
         ServerHttpRequest request = exchange.getRequest();
         String path = request.getPath().toString();
-        InetSocketAddress address = exchange.getRequest().getRemoteAddress();
+        InetSocketAddress address = request.getRemoteAddress();
+        HttpMethod method = request.getMethod();
 
         Mono<Void> mono = chain.filter(exchange);
 
         long duration = System.currentTimeMillis() - startTime;
-        log.info("gateway.port:{}, remoteAddress:{}, path:{}, time:{}", serverPort, address, path, duration);
+        ServerHttpResponse response = exchange.getResponse();
+        HttpStatusCode statusCode = response.getStatusCode();
+        log.info("gateway.port:{}, remoteAddress:{}, statusCode:{}", serverPort, address, statusCode);
+        log.info("path:{}, method:{}, time:{}", path, method, duration);
 
         return mono;
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 }
